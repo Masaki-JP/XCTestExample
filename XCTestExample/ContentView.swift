955eb0b8b3897c0495b440d48b4af224356aef9c
Masaki-JP <masakidayo0508@gmail.com>
2024-08-15 18:36:58 +0900

    SwiftUIのターゲットを追加

---
+++ XCTestExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XCTestExample
+//
+//  Created by Masaki Doi on 2024/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCTestExample/XCTestExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestExampleApp.swift
+//  XCTestExample
+//
+//  Created by Masaki Doi on 2024/08/15.
+//
+
+import SwiftUI
+
+@main
+struct XCTestExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
